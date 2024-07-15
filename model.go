cb2122588d03cba570d51548c9076ef78c8cac3b
onipicotte <onipicotte@gmail.com>
2024-07-15 17:42:08 -0400

    updated call structure, need to implement time

--- model.go
+++ model.go
@@ -2,37 +2,48 @@ package main
 
 import (
 	"database/sql"
-	"errors"
 )
 
 type call struct {
 	ID     int    `json:"id"`
 	Caller string `json:"caller"`
+	Recipient string `json:"recipient"`
 	Status string `json:"status"`
+	StartTime string `json:"startTime"`
+	EndTime string `json:"endTime"`
 }
 
+// Get one call
 func (c *call) getCall(db *sql.DB) error {
 	return db.QueryRow(
-		"SELECT id, caller, status FROM calls WHERE id=$1",
+		"SELECT id, caller, recipient, status, start_time, end_time FROM calls WHERE id=$1",
 		c.ID,
 	).Scan(
 		&c.ID, 
 		&c.Caller, 
+		&c.Recipient, 
 		&c.Status,
+		&c.StartTime, 
+		&c.EndTime, 
 	)
 }
 
+// Update one call
 func (c *call) updateCall(db *sql.DB) error {
 	_, err := db.Exec(
-		"UPDATE calls SET caller=$1, status=$2 WHERE id=$3",
+		"UPDATE calls SET caller=$1, recipient=$2, status=$3, start_time=$4, end_time=$5 WHERE id=$6",
 		c.Caller, 
+		c.Recipient,
 		c.Status,
+		c.StartTime,
+		c.EndTime,
 		c.ID, 
 	)
 
 	return err
 }
 
+// Delete one call
 func (c *call) deleteCall(db *sql.DB) error {
 	_, err := db.Exec(
 		"DELETE FROM calls WHERE id=$1",
@@ -42,12 +53,16 @@ func (c *call) deleteCall(db *sql.DB) error {
 	return err
 }
 
+// Create one call
 func (c *call) createCall(db *sql.DB) error {
 	err := db.QueryRow(
-		"INSERT INTO calls(caller, status) VALUES($1, $2) RETURNING id",
+		"INSERT INTO calls(caller, recipient, status, start_time, end_time) VALUES($1, $2, $3, $4, $5) RETURNING id",
 		c.Caller, 
+		c.Recipient, 
 		c.Status,
-	).Scan(&c.ID)
+		c.StartTime, 
+		c.EndTime, 
+		).Scan(&c.ID)
 
 	if err != nil {
 		return err
