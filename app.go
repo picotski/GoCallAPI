4512b08aeef01d398c1c6b680faba0c6131ebadf
onipicotte <onipicotte@gmail.com>
2024-07-15 17:20:35 -0400

    format code

--- app.go
+++ app.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"log"
 
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
@@ -9,11 +11,25 @@ import (
 
 type App struct {
 	Router *mux.Router
-	DB *sql.DB
+	DB     *sql.DB
 }
 
 func (a *App) Initialize(user, password, dbName string) {
+	connectionString := fmt.Sprintf(
+		"user=%s password=%s dbname=%s sslmode=disable",
+		user,
+		password,
+		dbName,
+	)
 
+	var err error
+
+	a.DB, err = sql.Open("postgres", connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	a.Router = mux.NewRouter()
 }
 
 func (a *App) Run(address string) {
