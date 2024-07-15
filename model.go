715dac53aa07601cfb8d7013f2df52982a6ddd17
onipicotte <onipicotte@gmail.com>
2024-07-15 17:28:02 -0400

    implemted getCall from the db

--- model.go
+++ model.go
@@ -15,11 +15,22 @@ func (c *call) getCall(db *sql.DB) error {
 	return db.QueryRow(
 		"SELECT id, caller, status FROM calls WHERE id=$1",
 		c.ID,
-	).Scan(&c.ID, &c.Caller, &c.Status)
+	).Scan(
+		&c.ID, 
+		&c.Caller, 
+		&c.Status,
+	)
 }
 
 func (c *call) updateCall(db *sql.DB) error {
-	return errors.New("Not implemented")
+	_, err := db.Exec(
+		"UPDATE calls SET caller=$1, status=$2 WHERE id=$3",
+		c.Caller, 
+		c.Status,
+		c.ID, 
+	)
+
+	return err
 }
 
 func (c *call) deleteCall(db *sql.DB) error {
